9d4a8e080e57662b3afb5b35e4b576be95adc2c4
Thejineaswar Guhan <67729644+Thejineaswar@users.noreply.github.com>
2022-10-30 21:44:11 +0530

    Update app.py

--- ML/API/backend/app.py
+++ ML/API/backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import request,jsonify
+import json
 import pickle
 from helper import *
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+
 
 app = Flask(__name__)
 
@@ -54,13 +56,23 @@ with open('../Data/ECG_data.npy', 'rb') as f:
 # Qquerying
 @app.route('/feature1',methods=['POST'])
 def procedure_costs():
-    # body = json.loads(request.data)
-    encounter_price = encounter_inference(encounter_data.iloc[[0]],encounter_data_scaler,encounter_model)
-    proc_medi = calculate_medicine_and_procedure_cost(medicine_proc_data.iloc[[0]],price_procedure_data_scaler,
+    body = json.loads(request.data)
+    enc_record = encounter_data[encounter_data['PATIENT'] == body['patient_id']]
+    med_record = medicine_proc_data[medicine_proc_data['PATIENT'] == body['patient_id']]
+
+    # encounter_price = encounter_inference(encounter_data.iloc[[0]],encounter_data_scaler,encounter_model)
+    # proc_medi = calculate_medicine_and_procedure_cost(medicine_proc_data.iloc[[0]],price_procedure_data_scaler,
+    #                                                   price_procedure_target_scaler,price_procedure_model,
+    #                                                   price_medication_data_scaler,price_medication_target_scaler,
+    #                                                   price_medication_model
+    #             )
+
+    encounter_price = encounter_inference(enc_record,encounter_data_scaler,encounter_model)
+    proc_medi = calculate_medicine_and_procedure_cost(med_record,price_procedure_data_scaler,
                                                       price_procedure_target_scaler,price_procedure_model,
                                                       price_medication_data_scaler,price_medication_target_scaler,
                                                       price_medication_model
-                )
+    )
     return jsonify(
         total_cost = int(encounter_price + proc_medi)
     )
@@ -68,8 +80,14 @@ def procedure_costs():
 
 @app.route('/feature2', methods=['POST'])
 def patient_history():
-    pred_1 = patient_history_inference(patient_history_data.iloc[[0]], rehospitalisation_data_scaler,
+    body = json.loads(request.data)
+    record = patient_history_data[patient_history_data['PATIENT'] == body['patient_id']]
+    # pred_1 = patient_history_inference(patient_history_data.iloc[[0]], rehospitalisation_data_scaler,
+    #                                  rehospitalisation_model)[0]
+
+    pred_1 = patient_history_inference(record, rehospitalisation_data_scaler,
                                      rehospitalisation_model)[0]
+
     pred_2 = ecg_inference(arr[0:50],model)
     output = (0.4 * pred_1) + (0.6 * pred_2)
     if output > 0.8:
